6b58661c5c0c218bf310e6494d47c34ab7cb92a4
swfoundation <266610886+starplatinumgit@users.noreply.github.com>
2026-06-27 14:38:49 +1000

    implement working reactive number button, operation button, AC, C, functionality (exclude decimal and equals

--- calculator.js
+++ calculator.js
@@ -1,52 +1,65 @@
 const numberButtons = document.querySelectorAll(".number-button");
-const operatorButtons = document.querySelectorAll(".operator-buttons");
+const operatorButtons = document.querySelectorAll(".operator-button");
 
-function add(a, b) {
-    return a + b;
+const operators = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b, 
+    divide: (a, b) => a / b,
 }
 
-function subtract(a, b) {
-    return a -b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-const isEmpty = (str) => str.length = 0;
+const isEmpty = (str) => str.length === 0;
 
 const operation = {
     numberA: "",
     numberB: "",
     operator: "",
     pastResult: "",
+    wipeCurrentOperation: function() {
+        operation.numberA = "";
+        operation.numberB = "";
+        operation.operator = "";
+    }
 }
 
 
 function operate(a, b, operator) {
-    return operator(a, b);
+    return operators[operator](a, b);
 }
 
 numberButtons.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        if (!(operation.operator)) {
+        if (isEmpty(operation.operator)) {
             operation.numberA += e.target.value;
+        }
+        else if (!(isEmpty(operation.operator))) {
+            operation.numberB += e.target.value;
+        }
+        else {
+            console.log("error!");
         }
     })
 })
 
 operatorButtons.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        if (isEmpty(operation.numberA) || isEmpty(operation.pastResult)) {
+        if (isEmpty(operation.numberA) && isEmpty(operation.pastResult)) {
+            console.log("1");
             return;
         }
-        else if (!(isEmpty(operation.numberB))) {
-            operation.pastResult(operate(operation.numberA, operation.numberB, operation.operator));
-            
+        else if (!(isEmpty(operation.numberA)) && isEmpty(operation.numberB)) {
+            console.log("2");
+            operation.operator = e.target.value;
+            console.log("triggered");
+        }
+        else if (!(isEmpty(operation.numberB)) && !(isEmpty(operation.numberA))) {
+            console.log("3");
+            operation.pastResult = (operate(operation.numberA, operation.numberB, operation.operator));
+            operation.wipeCurrentOperation();
+        }
+        else {
+            console.log("4");
         }
     })
 })
+
